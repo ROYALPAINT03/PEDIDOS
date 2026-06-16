980d9080c052d7c80b31f8945740d33f7ea4001b
ROYALPAINT03 <royaloficina3@gmail.com>
2026-06-16 17:05:22 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,6 @@
 // No lo elimines ni lo agregues a Git para evitar exponer claves en repositorios públicos.
 
 window.SUPABASE_CONFIG = {
-  url: "", // Introduce tu URL de Supabase aquí (ej. https://xxxx.supabase.co)
-  anonKey: "" // Introduce tu Anon Publishable Key aquí
+  url: "https://uxtxwoyfuinrmgqgkbzv.supabase.co/rest/v1/", // Introduce tu URL de Supabase aquí (ej. https://xxxx.supabase.co)
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4dHh3b3lmdWlucm1ncWdrYnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1ODU0MjIsImV4cCI6MjA5NzE2MTQyMn0.TfOYnWY2ZiE9jcdRqZJ9vql6Hs8xB4GOlu1Binfy5_w" // Introduce tu Anon Publishable Key aquí
 };
